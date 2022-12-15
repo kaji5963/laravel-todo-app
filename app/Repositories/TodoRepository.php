d9dd2172d68f0f41ac70538a03f78e10a2cb3ea7
takuya_kajiwara <fd.elt.119@gmail.com>
2022-12-15 20:51:56 +0900

    service層及びrepository層作成、controller処理切り分け

---
+++ app/Repositories/TodoRepository.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Todo;
+use Illuminate\Support\Facades\Auth;
+
+class TodoRepository
+{
+  /**
+   * dbからログインユーザーidで絞り込みデータを取得
+   *
+   * @return todos
+   */
+  public static function repository_get()
+  {
+    $user_id = Auth::user()->id;
+    $todos = Todo::where('user_id', '=', $user_id)->orderBy('id', 'desc')->get();
+
+    return $todos;
+  }
+
+  /**
+   * 新規todoをcreate処理
+   */
+  public static function repository_create($request)
+  {
+    $user_id = Auth::user()->id;
+
+    Todo::create([
+      'task' => $request->task,
+      'user_id' => $user_id,
+    ]);
+  }
+
+  /**
+   * dbからparamのidで詳細データ取得
+   *
+   * @return showTodo
+   * @param id
+   */
+  public static function repository_show($id)
+  {
+    $showTodo = Todo::find($id);
+
+    return $showTodo;
+  }
+
+  /**
+   * dbからparamのidで編集データ取得
+   *
+   * @return editTodo
+   * @param id
+   */
+  public static function repository_edit($id)
+  {
+    $editTodo = Todo::find($id);
+
+    return $editTodo;
+  }
+
+  /**
+   * 既存todoの編集処理
+   *
+   * @param request
+   * @param id
+   */
+  public static function repository_update($request, $id)
+  {
+    $updateTodo = Todo::find($id);
+    $updateTodo->task = $request->task;
+    $updateTodo->save();
+  }
+
+  /**
+   * dbからparamのidで削除データ取得
+   *
+   * @param id
+   */
+  public static function repository_delete($id)
+  {
+    $deleteTodo = Todo::find($id);
+
+    return $deleteTodo;
+  }
+
+  /**
+   * 既存todoの削除処理
+   *
+   * @param id
+   */
+  public static function repository_destroy($id)
+  {
+    Todo::find($id)->delete();
+  }
+}
